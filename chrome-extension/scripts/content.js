02113a0321bebe45ef12966eafa2b059ebecee9c
Manuel Doncel Martos <manuel.doncel.martos@ing.com>
2025-01-30 13:40:41 +0100

    reacting to new stories added on mutation

--- chrome-extension/scripts/content.js
+++ chrome-extension/scripts/content.js
@@ -4,41 +4,53 @@ const sprintTableBody = document.querySelector('tbody');
 
 // `document.querySelector` may return null if the selector doesn't match anything.
 if (sprintTableBody) {
-    let stories = sprintTableBody.querySelectorAll('tr.full-height')
+    const observer = new MutationObserver(mutationList =>
+        mutationList.filter(m => m.type === 'childList').forEach(m => {
+            m.addedNodes.forEach(x => {
+                if (x.nodeName === 'TR') {
+                    addDoubleClickListenerToTasks(x)
+                }
 
+            });
+        }));
+    observer.observe(sprintTableBody,{childList: true, subtree: true});
+
+    const stories = sprintTableBody.querySelectorAll('tr.full-height')
     for (let i = 0; i < stories.length; i++) {
-        // Story
-        let parent_story = stories[i].querySelector('td.taskboard-parent-cell').querySelector('div.card-content')
-        const parent_work_item_id = parent_story.querySelector('span.font-weight-semibold').textContent
-
-        // for each column, check tasks and create branch name
-        const tdColumns = stories[i].querySelectorAll('td.taskboard-expanded-cell')
-        for (let i = 0; i < tdColumns.length; i++) {
-            const column = tdColumns[i]
-            let divTasks = column.querySelectorAll('div.taskboard-card')
-
-            for (let j = 0; j < divTasks.length; j++) {
-                // Task in Column
-                const title = divTasks[j].querySelector('span.title-text').textContent
-                const gitTitle = replaceTaskTitle(title)
-                const branchName = `feature/${parent_work_item_id}/${gitTitle}`
-
-                // Attach button
-                const boltButton = divTasks[j].querySelector('button.bolt-icon-button')
-                boltButton.addEventListener('dblclick', () => {
-                    navigator.clipboard.writeText(branchName).then(
-                        () => {
-                            loadSnackbar(`Branch name '${branchName}' copied to clipboard`)
-                        }
-                    ).finally();
-                })
-
-
-            }
-        }
-
+        addDoubleClickListenerToTasks(stories[i])
     }
 
+}
+
+function addDoubleClickListenerToTasks(story) {
+    let parent_story = story.querySelector('td.taskboard-parent-cell').querySelector('div.card-content')
+    const parent_work_item_id = parent_story.querySelector('span.font-weight-semibold').textContent
+
+    // for each column, check tasks and create branch name
+    const tdColumns = story.querySelectorAll('td.taskboard-expanded-cell')
+    for (let i = 0; i < tdColumns.length; i++) {
+        const column = tdColumns[i]
+        let divTasks = column.querySelectorAll('div.taskboard-card')
+
+        for (let j = 0; j < divTasks.length; j++) {
+            // Task in Column
+            const title = divTasks[j].querySelector('span.title-text').textContent
+            const gitTitle = replaceTaskTitle(title)
+            const branchName = `feature/${parent_work_item_id}/${gitTitle}`
+
+            // Attach button
+            const boltButton = divTasks[j].querySelector('button.bolt-icon-button')
+            boltButton.addEventListener('dblclick', () => {
+                navigator.clipboard.writeText(branchName).then(
+                    () => {
+                        loadSnackbar(`Branch name '${branchName}' copied to clipboard`)
+                    }
+                ).finally();
+            })
+
+
+        }
+    }
 }
 
 function replaceTaskTitle(title) {
